6a2d5a2ab188b84ffec82775e3f43d03df0b9e0f
Aaron W Chen <awchen2009@gmail.com>
2019-09-17 14:56:04 -0700

    Update

--- sql/sorted_start_times.sql
+++ sql/sorted_start_times.sql
@@ -1,7 +1,14 @@
-SELECT c.name as course_name, CAST(start_time as float) / 60 AS start_time_in_hours, 
-    CAST(end_time as float) / 60 AS end_time_in_hours
-FROM schedules
-JOIN sections on schedules.uuid = sections.schedule_uuid 
-JOIN course_offerings as c on sections.course_offering_uuid = c.uuid
-GROUP BY course_name, start_time_in_hours, end_time_in_hours
-ORDER BY start_time_in_hours ASC;
+SELECT COUNT(*) 
+FROM 
+    (SELECT courses.name, 
+        CAST(start_time AS float) / 60 AS start_time_in_hours, 
+        CAST(end_time AS float) / 60 AS end_time_in_hours
+    FROM schedules
+    --WHERE start_time_in_hours > 0
+    JOIN sections ON schedules.uuid = sections.schedule_uuid 
+    JOIN course_offerings AS c ON sections.course_offering_uuid = c.uuid
+    JOIN courses ON c.course_uuid = courses.uuid
+    GROUP BY courses.uuid, courses.name, start_time_in_hours, end_time_in_hours
+    ORDER BY start_time_in_hours ASC
+    ) AS start_times_converted
+WHERE start_time_in_hours BETWEEN 0 AND 12 AND name != 'null';
---
+++ sql/time_of_day.sql
@@ -0,0 +1,7 @@
+/* Return the times of day 
+
+*/
+
+
+SELECT start_time
+FROM 
